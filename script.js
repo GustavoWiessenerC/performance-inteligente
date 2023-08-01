b558dd6cada048eefacff12d17c978957a10fc77
Gustavo Henrique de Castilho <gustavo.castilho@sulamerica.com.br>
2023-08-01 17:26:29 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+import { check } from 'k6';
+import http from 'k6/http';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export let options = {
+  vus: 10,
+  duration: '10s'
+};
+
+export default function () {
+  let res = http.get('http://test.k6.io/');
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+    'body contains text match': (r) => r.body.includes('Collection of simple web-pages suitable for load testing.'),
+  });
+  
+}
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data)
+  };
+}
