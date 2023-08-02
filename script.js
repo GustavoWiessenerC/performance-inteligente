2b6cde8a01a8b29cb032bde78c862d873e47a237
Gustavo Henrique de Castilho <gustavo.castilho@sulamerica.com.br>
2023-08-02 10:08:56 -0300

    Inclusão de report visualizacao - estrategias

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporte
 
 export let options = {
   vus: 10,
-  duration: '10s'
+  duration: '20s'
 };
 
 export default function () {
